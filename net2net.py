a8cc15aa94a4513819b4f73977bd7e9e959d4ce6
Eren Golge <eren.golge@twentybn.com>
2017-09-21 06:35:55 -0700

    First commit

---
+++ net2net.py
@@ -0,0 +1,213 @@
+import torch as th
+import numpy as np
+from collections import Counter
+
+
+def _wrap_net(nn):
+    return th.nn.ModuleList(nn._modules)
+
+
+def wider(m1, m2, new_width, bnorm=None, out_size=None):
+    """
+    Convert m1 layer to its wider version by adapthing next weight layer and
+    possible batch norm layer in btw.
+    Args:
+        m1 - module to be wider
+        m2 - follwing module to be adapted to m1
+        new_width - new width for m1.
+        bn (optional) - batch norm layer, if there is btw m1 and m2
+        out_size (list, optional) - necessary for m1 == conv3d and m2 == linear. It
+            is 3rd dim size of the output feature map of m1. Used to compute
+            the matching Linear layer size
+    """
+
+    w1 = m1.weight.data
+    w2 = m2.weight.data
+    b1 = m1.bias.data
+
+    if "Conv" in m1.__class__.__name__ or "Linear" in m1.__class__.__name__:
+        # Convert Linear layers to Conv if linear layer follows target layer
+        if "Conv" in m1.__class__.__name__ and "Linear" in m2.__class__.__name__:
+            assert w2.size(1) % w1.size(0) == 0, "Linear units need to be multiple"
+            if w1.dim() == 4:
+                factor = int(np.sqrt(w2.size(1) // w1.size(0)))
+                w2 = w2.view(w2.size(0), w2.size(1)//factor**2, factor, factor)
+            elif w1.dim() == 5:
+                assert out_size is not None,\
+                       "For conv3d -> linear out_size is necessary"
+                factor = out_size[0] * out_size[1] * out_size[2]
+                w2 = w2.view(w2.size(0), w2.size(1)//factor, out_size[0],
+                             out_size[1], out_size[2])
+        else:
+            assert w1.size(0) == w2.size(1), "Module weights are not compatible"
+        assert new_width > w1.size(0), "New size should be larger"
+
+        old_width = w1.size(0)
+        nw1 = m1.weight.data.clone()
+        nw2 = w2.clone()
+
+        if nw1.dim() == 4:
+            nw1.resize_(new_width, nw1.size(1), nw1.size(2), nw1.size(3))
+            nw2.resize_(nw2.size(0), new_width, nw2.size(2), nw2.size(3))
+        elif nw1.dim() == 5:
+            nw1.resize_(new_width, nw1.size(1), nw1.size(2), nw1.size(3), nw1.size(4))
+            nw2.resize_(nw2.size(0), new_width, nw2.size(2), nw2.size(3), nw2.size(4))
+        else:
+            nw1.resize_(new_width, nw1.size(1))
+            nw2.resize_(nw2.size(0), new_width)
+
+        if b1 is not None:
+            nb1 = m1.bias.data.clone()
+            nb1.resize_(new_width)
+
+        if bnorm is not None:
+            nrunning_mean = bnorm.running_mean.data.clone().resize_(new_width)
+            nrunning_var = bnorm.running_var.data.clone().resize_(new_width)
+            if bnorm.affine:
+                nweight = bnorm.weight.data.clone().resize_(new_width)
+                nbias = bnorm.bias.data.clone().resize_(new_width)
+
+        w2 = w2.transpose(0, 1)
+        nw2 = nw2.transpose(0, 1)
+
+        nw1.narrow(0, 0, old_width).copy_(w1)
+        nw2.narrow(0, 0, old_width).copy_(w2)
+        nb1.narrow(0, 0, old_width).copy_(b1)
+
+        if bnorm is not None:
+            nrunning_var.narrow(0, 0, old_width).copy_(bnorm.running_var)
+            nrunning_mean.narrow(0, 0, old_width).copy_(bnorm.running_mean)
+            if bnorm.affine:
+                nweight.narrow(0, 0, old_width).copy_(bnorm.weight)
+                nbias.narrow(0, 0, old_width).copy_(bnorm.bias)
+
+        # select weights randomly
+        tracking = dict()
+        for i in range(old_width, new_width):
+            idx = np.random.randint(0, old_width)
+            try:
+                tracking[idx].append(i)
+            except:
+                tracking[idx] = [idx]
+                tracking[idx].append(i)
+            nw1.select(0, i).copy_(w1.select(0, idx).clone())
+            nw2.select(0, i).copy_(w2.select(0, idx).clone())
+            nb1[i] = b1[idx]
+
+        if bnorm is not None:
+            nrunning_mean[i] = bnorm.running_mean[idx]
+            nrunning_var[i] = bnorm.running_var[idx]
+            if bnorm.affine:
+                nweight[i] = bnorm.weight[idx]
+                nbias[i] = bnorm.bias[idx]
+
+        for idx, d in tracking.items():
+            for item in d:
+                nw2[item].div_(len(d))
+
+        w2.transpose_(0, 1)
+        nw2.transpose_(0, 1)
+
+        m1.out_channels = new_width
+        m2.in_channels = new_width
+
+        m1.weight.data = nw1
+
+        if "Conv" in m1.__class__.__name__ and "Linear" in m2.__class__.__name__:
+            if w1.dim() == 4:
+                m2.weight.data = nw2.view(m2.weight.size(0), new_width*factor**2)
+                m2.in_features = new_width*factor**2
+            elif w2.dim() == 5:
+                m2.weight.data = nw2.view(m2.weight.size(0), new_width*factor)
+                m2.in_features = new_width*factor
+        else:
+            m2.weight.data = nw2
+
+        m1.bias.data = nb1
+
+        if bnorm is not None:
+            bnorm.running_var = nrunning_var
+            bnorm.running_mean = nrunning_mean
+            if bnorm.affine:
+                bnorm.weight = nweight
+                bnorm.bias = nbias
+        return m1, m2, bnorm
+
+
+def deeper(m, nonlin, bnorm_flag=False):
+    if "Linear" in m.__class__.__name__:
+        m2 = th.nn.Linear(m.out_features, m.out_features)
+        m2.weight.data.copy_(th.eye(m.out_features))
+        m2.bias.data.zero_()
+
+        if bnorm_flag:
+            bnorm = th.nn.BatchNorm1d(m2.weight.size(1))
+            bnorm.weight.data.fill_(1)
+            bnorm.bias.data.fill_(0)
+            bnorm.running_mean.fill_(0)
+            bnorm.running_var.fill_(1)
+
+    elif "Conv" in m.__class__.__name__:
+        assert m.kernel_size[0] % 2 == 1, "Kernel size needs to be odd"
+
+        if m.weight.dim() == 4:
+            pad_h = int((m.kernel_size[0] - 1) / 2)
+            # pad_w = pad_h
+            m2 = th.nn.Conv2d(m.out_channels, m.out_channels,
+                              kernel_size=m.kernel_size, padding=pad_h)
+            m2.weight.data.zero_()
+            c = m.kernel_size[0] // 2 + 1
+
+        elif m.weight.dim() == 5:
+            pad_hw = int((m.kernel_size[1] - 1) / 2)  # pad height and width
+            pad_d = int((m.kernel_size[0] - 1) / 2)  # pad depth
+            m2 = th.nn.Conv3d(m.out_channels,
+                              m.out_channels,
+                              kernel_size=m.kernel_size,
+                              padding=(pad_d, pad_hw, pad_hw))
+            c_wh = m.kernel_size[1] // 2 + 1
+            c_d = m.kernel_size[0] // 2 + 1
+
+        restore = False
+        if m2.weight.dim() == 2:
+            restore = True
+            m2.weight.data = m2.weight.data.view(m2.weight.size(0),
+                                                 m2.in_channels,
+                                                 m2.kernel_size[0],
+                                                 m2.kernel_size[0])
+
+        for i in range(0, m.out_channels):
+            if m.weight.dim() == 4:
+                m2.weight.data.narrow(0, i, 1).narrow(1, i, 1).narrow(2, c, 1).narrow(3, c, 1).fill_(1)
+            elif m.weight.dim() == 5:
+                m2.weight.data.narrow(0, i, 1).narrow(1, i, 1).narrow(2, c_d, 1).narrow(3, c_wh, 1).narrow(4, c_wh, 1).fill_(1)
+
+        if restore:
+            m2.weight.data = m2.weight.data.view(m2.weight.size(0),
+                                                 m2.in_channels,
+                                                 m2.kernel_size[0],
+                                                 m2.kernel_size[0])
+
+        m2.bias.data.zero_()
+
+        if bnorm_flag:
+            if m.weight.dim() == 4:
+                bnorm = th.nn.BatchNorm2d(m2.out_channels)
+            elif m.weight.dim() == 5:
+                bnorm = th.nn.BatchNorm3d(m2.out_channels)
+            bnorm.weight.data.fill_(1)
+            bnorm.bias.data.fill_(0)
+            bnorm.running_mean.fill_(0)
+            bnorm.running_var.fill_(1)
+
+    else:
+        raise RuntimeError("{} Module not supported".format(m.__class__.__name__))
+
+    s = th.nn.Sequential()
+    s.add_module('conv', m)
+    if bnorm_flag:
+        s.add_module('bnorm', bnorm)
+    s.add_module('nonlin', nonlin())
+    s.add_module('conv_new', m2)
+
+    return s
---
+++ tests.py
@@ -0,0 +1,139 @@
+import unittest
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+from net2net import wider, deeper
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, x.size(1)*x.size(2)*x.size(3))
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+
+class Net3D(nn.Module):
+    def __init__(self):
+        super(Net3D, self).__init__()
+        self.conv1 = nn.Conv3d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv3d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool3d(self.conv1(x), 2))
+        x = F.relu(F.max_pool3d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, x.size(1)*x.size(2)*x.size(3)*x.size(4))
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+
+class TestOperators(unittest.TestCase):
+
+    def _create_net(self):
+        return Net()
+
+    def test_wider(self):
+        net = self._create_net()
+        inp = th.autograd.Variable(th.rand(32, 1, 28, 28))
+
+        net.eval()
+        out = net(inp)
+
+        conv1, conv2, _ = wider(net._modules['conv1'],
+                                net._modules['conv2'],
+                                20)
+
+        net._modules['conv1'] = conv1
+        net._modules['conv2'] = conv2
+
+        conv2, fc1, _ = wider(net._modules['conv2'],
+                              net._modules['fc1'],
+                              60)
+        net._modules['conv2'] = conv2
+        net._modules['fc1'] = fc1
+
+        net.eval()
+        nout = net(inp)
+        assert th.abs((out - nout).sum().data)[0] < 1e-1
+        assert nout.size(0) == 32 and nout.size(1) == 10
+
+        # Testing 3D layers
+        net = Net3D()
+        inp = th.autograd.Variable(th.rand(32, 1, 16, 28, 28))
+
+        net.eval()
+        out = net(inp)
+
+        conv1, conv2, _ = wider(net._modules['conv1'],
+                                net._modules['conv2'],
+                                20)
+
+        net._modules['conv1'] = conv1
+        net._modules['conv2'] = conv2
+
+        conv2, fc1, _ = wider(net._modules['conv2'],
+                              net._modules['fc1'],
+                              60,
+                              out_size=[1, 4, 4])
+        net._modules['conv2'] = conv2
+        net._modules['fc1'] = fc1
+
+        net.eval()
+        nout = net(inp)
+        assert th.abs((out - nout).sum().data)[0] < 1e-1
+        assert nout.size(0) == 32 and nout.size(1) == 10
+
+    def test_deeper(self):
+        net = self._create_net()
+        inp = th.autograd.Variable(th.rand(32, 1, 28, 28))
+
+        net.eval()
+        out = net(inp)
+
+        s = deeper(net._modules['conv1'], nn.ReLU, bnorm_flag=True)
+        net._modules['conv1'] = s
+
+        s2 = deeper(net._modules['conv2'], nn.ReLU, bnorm_flag=True)
+        net._modules['conv2'] = s2
+
+        s3 = deeper(net._modules['fc1'], nn.ReLU, bnorm_flag=False)
+        net._modules['fc1'] = s3
+
+        net.eval()
+        nout = net(inp)
+
+        assert th.abs((out - nout).sum().data)[0] < 1e-1
+
+        # test for 3D net
+        net = Net3D()
+        inp = th.autograd.Variable(th.rand(32, 1, 16, 28, 28))
+
+        net.eval()
+        out = net(inp)
+
+        s = deeper(net._modules['conv1'], nn.ReLU, bnorm_flag=True)
+        net._modules['conv1'] = s
+
+        s2 = deeper(net._modules['conv2'], nn.ReLU, bnorm_flag=True)
+        net._modules['conv2'] = s2
+
+        net.eval()
+        nout = net(inp)
+
+        assert th.abs((out - nout).sum().data)[0] < 1e-1
